230e29c480633c7e9791b0b28e5a4eaf833cc810
Wasanta <kwan_u_day@hotmail.com>
2021-11-02 16:19:08 -0400

    compleated

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUp){
+    if (pickUp > 42){
+        return pickUp - 42
+    }else{
+        return 42-pickUp
+    }     
+}
+
+function distanceFromHqInFeet(pickUp){
+    if (pickUp > 42){
+        return (pickUp - 42)*264
+    }else{
+        return (42-pickUp)*264
+    } 
+}
+
+function distanceTravelledInFeet(start, end){
+    if (start < 42){
+        return(start-end)*264;
+    } else{
+        return(end - start)*264;
+    }  
+}
+
+function calculatesFarePrice(start,end){
+    const feet = distanceTravelledInFeet(start, end)
+    if (feet <=  400){
+        return 0
+    }else if (feet > 400 && feet <=2000 ){
+        return (feet - 400) * .02 
+    }else if (feet > 2000 && feet < 2500){
+        return 25
+    }else{
+        return 'cannot travel that far'
+    }
+} 
